47200e80881172bde7722015e9540a62bc36abc8
hoomanist <hham.group12@gmail.com>
2020-07-26 19:33:27 +0430

    polybar is better

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar example &
+polybar panel &
+
 
 echo "Polybar launched..."
