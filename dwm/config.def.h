92cd49ba284b33cdaf083b3e6044671f6418817a
hoomanist <hoomanist86@protonmail.com>
2020-10-27 03:14:53 +0330

    change color

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 5;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 0;       /* vertical padding of bar */
+static const int sidepad            = 0;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Iosevka:size=10" };
 static const char dmenufont[]       = "Iosevka:size=10";
 static const char col_gray1[]       = "#1d1f21";
