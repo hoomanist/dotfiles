c5f8ed0bb7813d5e02a0c6e8ed18fdd7ac620f70
hoomanist <hham.group12@gmail.com>
2021-09-04 13:25:32 +0430

    new nixos configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,156 +1,93 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
-
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
-
-  # Use the systemd-boot EFI boot loader.
+  # bootloader
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.device = "nodev";
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
-  networking.hostName = "death-star"; # Define your hostname.
-  networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
-
-  nixpkgs.config.allowUnfree = true;
-  # Set your time zone.
-  time.timeZone = "Asia/Tehran";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+  # filesystems
+  boot.supportedFilesystems = [ "ntfs" ];
+  # networking
+  networking.hostName = "pandemonium";
+  networking.networkmanager.enable = true;  
   networking.useDHCP = false;
   networking.interfaces.enp1s0.useDHCP = true;
   networking.interfaces.wlp2s0.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
-  # internet
-  networking.networkmanager.enable = true;  
-  # enable xorg
-  services.xserver.enable = true;
-  # Configure keymap in X11
-  services.xserver.layout = "us, ir";
-  services.xserver.xkbOptions = "grp:alt_shift_toggle";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-  # window manager
-  #services.xserver.windowManager.i3 = {
-  #  enable = true;
-  #  package = pkgs.i3-gaps;
-  #};
-  #services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome3.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.hooman= {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    shell = pkgs.fish;
-  };
+  #tor
   services.tor.enable = true;
   services.tor.client.enable = true;
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-	# coding
-  	neovim
-	tmux
-	go
-	acpi
-	git
-	nodejs
-	clang
-	gcc
-	python3
-	postman
-	# terminal
-	wget
-	htop
-	alacritty
-	termite
-	neofetch
-	irssi
-	#tor
-	scrot
-	ncmpcpp
-	# gui
-	papirus-icon-theme
-  	firefox
-	gnome3.gnome-tweaks
-	emacs
-	pcmanfm	
-	tdesktop
-	discord
-	zathura
-  ];
-  fonts.fonts = with pkgs; [
-	iosevka-bin
-	fantasque-sans-mono
-  	fira-code
-  	fira-code-symbols
-  ];
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
-  fonts.fontconfig.enable = true;
-  services.mpd.enable = true;
-
-  # mysql
-  services.mysql = {
-      enable = true;
-      dataDir = "/var/db/mysql";
-      package = pkgs.mysql;
+  # timezone
+  time.timeZone = "Asia/Tehran";
+  # locale
+  i18n.defaultLocale = "en_US.UTF-8";
+  #xorg
+  services.xserver = {
+     enable = true;
+     layout = "us, ir";
+     xkbOptions = "grp:alt_shift_toggle";
+     displayManager.lightdm.enable = true;
+     windowManager.bspwm.enable = true;
+  }; 
+  #sound
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  #touchpad
+  services.xserver.libinput.enable = true;
+  #user
+  users.users.hooman = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "network" "storage" ];
+    shell = pkgs.fish;
   };
+  # packages
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+    wget
+    firefox
+    alacritty
+    rofi
+    neofetch
+    tmux
+    lemonbar
+    acpi
+    nitrogen
+    pfetch
+    unzip
+    vlc
+    tdesktop
+    xfce.thunar
+    xfce.thunar-volman
+    xfce.xfconf
+    xfce.tumbler 
+    xfce.exo
+    xdotool
+    wirelesstools
+    rustup
+    rust-analyzer
+  ];
+  #fonts
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "Iosevka" ]; })
+  ];
+  # gpg
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  # using neovim nightly
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+  # version of the system
+  system.stateVersion = "21.05";
 
 }
-
